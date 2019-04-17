f992dc2646c8ae749a36b177bc894a7443d35884
yusiyao <49708458+yusiyao@users.noreply.github.com>
2019-04-17 18:20:06 +0800

    Update Test.java

--- src/main/java/com/baizhi/Test.java
+++ src/main/java/com/baizhi/Test.java
@@ -3,5 +3,6 @@ package com.baizhi;
 public class Test {
     public void test(){
         System.out.println("this is first gitTest");
+        system.ot.println("-----------------------");
     }
 }
