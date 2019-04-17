6cb3d0100625d3c63675fbe7a382cf9b4bd872e0
yaya <15110295697@163.com>
2019-04-17 18:26:41 +0800

    git-test create

--- src/main/java/com/baizhi/Test.java
+++ src/main/java/com/baizhi/Test.java
@@ -3,6 +3,7 @@ package com.baizhi;
 public class Test {
     public void test(){
         System.out.println("this is first gitTest");
-        system.ot.println("-----------------------");
+        System.out.println("-----------------------");
+        System.out.println("=================================");
     }
 }
